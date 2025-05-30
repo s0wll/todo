2c46f51507e17cadd843afd7a1e6236442bfa1ec
Mark Ten <markten2002@gmail.com>
2025-05-30 23:32:02 +0300

    feat: deploy project to docker and make docker-compose file

--- backend/src/main.py
+++ backend/src/main.py
@@ -21,4 +21,4 @@ app.include_router(router_tasks)
 
 if __name__ == "__main__":
     logging.info("Запуск приложения через uvicorn")
-    uvicorn.run("main:app", host="", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
